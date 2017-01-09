1b52cfbb1f49ba9b3078c492b2e01a3ebd34f980
John Smith <xfgryujk@126.com>
2017-01-09 19:10:17 +0800

    改掉脑残写法

--- Plugin/DesktopBrowser/Config.cpp
+++ Plugin/DesktopBrowser/Config.cpp
@@ -14,7 +14,7 @@ Config::Config()
 void Config::LoadConfig(LPCWSTR path)
 {
 	m_homePage.resize(2083);
-	GetPrivateProfileStringW(APPNAME, L"HomePage", L"https://github.com/xfgryujk/WinCustomDesktop", (LPWSTR)m_homePage.c_str(), 
-		(DWORD)m_homePage.size(), path);
+	GetPrivateProfileStringW(APPNAME, L"HomePage", L"https://github.com/xfgryujk/WinCustomDesktop", &m_homePage.front(), 
+		static_cast<DWORD>(m_homePage.size()), path);
 	m_homePage.resize(wcslen(m_homePage.c_str()));
 }
