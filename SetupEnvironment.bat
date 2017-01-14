eec9d05e8d5c0a44e8db50dfe09e974feb190e9b
John Smith <xfgryujk@126.com>
2017-01-14 10:15:33 +0800

    改掉脑残写法

--- SetupEnvironment.bat
+++ SetupEnvironment.bat
@@ -2,16 +2,17 @@
 
 set DIRS=Win32\Debug Win32\Release x64\Debug x64\Release
 
-for %%i in (%DIRS%) do md %%i\Plugin
-for %%i in (%DIRS%) do xcopy Data %%i\Plugin /e /y
-for %%i in (%DIRS%) do ^
-echo [VideoDesktop] > %%i\Plugins.ini && ^
-echo Enable=0 >> %%i\Plugins.ini && ^
-echo [DesktopBrowser] >> %%i\Plugins.ini && ^
-echo Enable=1 >> %%i\Plugins.ini && ^
-echo [MaskDesktop] >> %%i\Plugins.ini && ^
-echo Enable=0 >> %%i\Plugins.ini && ^
-echo [WIMC] >> %%i\Plugins.ini && ^
+for %%i in (%DIRS%) do (
+md %%i\Plugin\Data
+xcopy Data %%i\Plugin\Data /e /y
+echo [VideoDesktop] > %%i\Plugins.ini
+echo Enable=0 >> %%i\Plugins.ini
+echo [DesktopBrowser] >> %%i\Plugins.ini
 echo Enable=1 >> %%i\Plugins.ini
+echo [MaskDesktop] >> %%i\Plugins.ini
+echo Enable=0 >> %%i\Plugins.ini
+echo [WIMC] >> %%i\Plugins.ini
+echo Enable=1 >> %%i\Plugins.ini
+)
 
 pause
